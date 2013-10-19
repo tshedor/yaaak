244b3ea702e8296846f44dcc2e2e8ad64536940a
Kyle <kyle@brocktoncg.com>
2013-10-18 20:06:16 -0500

    blank rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yak::Application.config.secret_token = '839cf0c828ab8fe0ce3468ea718f0806e034a2504cf25c291dce5868c41e317c1e950b6d42219a529b267506925bb73faa4ef6b58b2fbb8b77d7b17ec558da8c'
