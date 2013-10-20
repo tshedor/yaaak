9d6657d90e54602e1a45343dc95ddb48d55dee76
John Kellyn Resman <jkresman@ksu.edu>
2013-10-20 15:07:07 -0500

    Overlay

---
+++ app/assets/javascripts/overlay.js
@@ -0,0 +1,30 @@
+window.toggle_overlay = () ->
+	content = $('.overlay-content')
+	if content.hasClass('overlayShowing')
+		$('body').removeClass('noscroll')
+		$('.overlay, .overlay-bg').hide()
+		content.removeClass('overlayShowing')
+	else
+		$('body').addClass('noscroll')
+		$('.overlay, .overlay-bg').show()
+		content.addClass('overlayShowing')
+		$(".btn").button('reset')
+		center_content()
+		overlay_resize()
+
+overlay_resize = () ->
+	maskHeight = $(document).height()
+	maskWidth = $(window).width()
+	$('.overlay-bg').css({ 'width':maskWidth,'height':maskHeight })
+
+window.center_content = () ->
+	top = Math.max($(window).height() - $('.overlay').outerHeight(), 0) / 2
+	left = Math.max($(window).width() - $('.overlay').outerWidth(), 0) / 2
+	$('.overlay').css({ top:top + $(window).scrollTop(), left:left + $(window).scrollLeft() })
+
+$('.overlay-content .destroy').live 'click', () -> toggle_overlay()
+$('.overlay-bg').live 'click', () -> toggle_overlay()
+
+$(window).resize () -> 
+	center_content()
+	overlay_resize()
