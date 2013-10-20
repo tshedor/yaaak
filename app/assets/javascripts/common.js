3bf23e358172434f334534df5b2095cd726ac915
Tim Shedor <tshedor@gmail.com>
2013-10-19 20:32:24 -0500

    only call herds if on a herds page

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,5 +1,5 @@
   $(document).ready(function() {
-    
+
     $('select').selectpicker();
 
     $('#show-hdr').click(function(e) {
@@ -8,34 +8,40 @@
       $('.hdr').toggleClass('hdr-out');
       return $('.hdr-controls').toggle().toggleClass('flipInY animated');
     });
-    
+
     $('body.herds').backstretch(['/assets/herds-bg.jpg']);
 
 /**********************/
 /******* HERDS ********/
 /**********************/
 
-    var evtSource, isEmpty;
-    evtSource = new EventSource('/herds/1/stream');
-    isEmpty = function(str) {
-      return !str || 0 === str.length;
-    };
-    evtSource.onmessage = function(e) {
-      var resp;
-      resp = JSON.parse(e.data);
-      $('.chat-list').append('<li>' + resp.data.message + '</li>');
-      return console.log(resp);
-    };
-    evtSource.onopen = function(e) {
-      return console.log('open');
-    };
-    evtSource.onerror = function(e) {
-      console.log('error');
-      return console.log(e);
-    };
-	evtSource.onclose = function(e) {
-      return console.log('close');
-    };
+	if($('body').hasClass('herds')){
+    	var evtSource, isEmpty;
+    	evtSource = new EventSource('/herds/1/stream');
+    	isEmpty = function(str) {
+    	  return !str || 0 === str.length;
+    	};
+    	evtSource.onmessage = function(e) {
+    	  var resp;
+    	  resp = JSON.parse(e.data);
+    	  $('.chat-list').append('<li>' + resp.data.message + '</li>');
+    	  return console.log(resp);
+    	};
+    	evtSource.onopen = function(e) {
+    	  return console.log('open');
+    	};
+    	evtSource.onerror = function(e) {
+    	  console.log('error');
+    	  return console.log(e);
+    	};
+		evtSource.onclose = function(e) {
+    	  return console.log('close');
+    	};
+
+		window.onbeforeunload = function(){
+			evtSource.close()
+		}
+	}
 
 /**********************/
 /******* GRUNTS *******/
