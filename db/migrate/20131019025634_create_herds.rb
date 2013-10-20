5bdad0ea0c84aa268b6343bc02615b9c8b6438f1
Kyle <kyle@brocktoncg.com>
2013-10-20 03:37:51 -0500

    herd creation

--- db/migrate/20131019025634_create_herds.rb
+++ db/migrate/20131019025634_create_herds.rb
@@ -2,6 +2,7 @@ class CreateHerds < ActiveRecord::Migration
 
   def change
     create_table :herds do |t|
+      t.integer :parent_id
       t.float :geo_lat
       t.float :geo_long
       t.timestamps
