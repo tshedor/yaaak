3cead84a727aaa1718327b6e6036120fd06e85c9
Kyle <kyle@brocktoncg.com>
2013-10-20 14:10:14 -0500

    yaks function for each herd - auto assign herds

--- app/models/grunt.rb
+++ app/models/grunt.rb
@@ -5,14 +5,26 @@ class Grunt < ActiveRecord::Base
   has_many :herd_grunts, :dependent => :destroy
   has_many :herds, :through => :herd_grunts
 
-  # after_save :attach_or_create_herd
+  after_save :attach_or_create_herd
+
+  def display_name
+    if yak && yak.name
+      yak.name.split(' ')[1] ? yak.name.split(' ')[0][0] + yak.name.split(' ')[1][0] : yak.name.split(' ')[0][0]
+    else
+      'Unknown'
+    end
+  end
+
+  def master_herd
+    herds.first.master_herd if herds.any?
+  end
 
   def self.nearest_grunt(lat, long, mile_radius = 5)
     Grunt.find_by_sql("
       SELECT id, geo_lat, geo_long, created_at,
       ( 3959 * acos( cos( radians( #{lat} ) ) * cos( radians( geo_lat ) ) * cos( radians( geo_long ) - radians( #{long} ) ) + sin( radians( #{lat} ) ) * sin( radians( geo_lat ) ) ) ) AS distance
       FROM grunts HAVING distance < #{mile_radius} ORDER BY distance LIMIT 1
-    ")
+    ").first
   end
 
   def self.delete_old_grunts
@@ -23,11 +35,13 @@ private
 
   def attach_or_create_herd
     nearest_grunt = Grunt.nearest_grunt(geo_lat, geo_long)
-    if nearest_grunt
-      nearest_grunt.herd.grunts << self
+    if nearest_grunt && nearest_grunt.master_herd
+      herd = nearest_grunt.master_herd
     else
-      Herd.create(geo_lat: geo_lat, geo_long: geo_long).grunts << self
+      herd = Herd.create(geo_lat: geo_lat, geo_long: geo_long)
     end
+    herd.grunts << self
+    yak.update_attributes(herd_id: herd.id, geo_lat: geo_lat, geo_long: geo_long) if yak
   end
 
 end
